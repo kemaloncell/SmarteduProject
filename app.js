b8b5ad2033aa1d135297f02e5d07c1a2f890161c
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-08 17:46:14 +0300

    partials folder added and some package installed

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// Template engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.status(200).send('index');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('page of about');
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = 3000;
