4c783ec408058c0005db5c611b3f93207f42611b
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-09 00:43:36 +0300

    mvc structure created and route folder is created

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/PageRoute');
+const courseRoute = require('./routes/CourseRoute');
 const app = express();
 
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+  console.log('DB conntected Successfuly');
+});
 // Template engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
