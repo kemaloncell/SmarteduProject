d764f80c5f88191930f60fec56f5599b3bb0b166
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-10 01:40:36 +0300

    login page created

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -10,8 +10,14 @@ exports.getAboutPage = (req, res) => {
   });
 };
 
-exports.getUserPage = (req, res) => {
+exports.getRegisterPage = (req, res) => {
   res.status(200).render('register', {
     page_name: 'register',
+  });
+};
+
+exports.getLoginPage = (req, res) => {
+  res.status(200).render('login', {
+    page_name: 'login',
   });
 };
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/AuthController');
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); // localhost:3000/users/signup
+router.route('/login').post(authController.loginUser); // localhost:3000/users/signup
 
 module.exports = router;
