af435e0a95c2764a1d1c5a17df392ff4c7730c9f
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-09 19:58:11 +0300

    Categories were  created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/PageRoute');
 const courseRoute = require('./routes/CourseRoute');
+const categoryRoute = require('./routes/CategoryRoute');
 const app = express();
 
 mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
 
 const port = 3000;
 app.listen(port, () => {
--- routes/CourseRoute.js
+++ routes/CourseRoute.js
@@ -4,5 +4,6 @@ const courseController = require('../controllers/CourseController');
 const router = express.Router();
 
 router.route('/').post(courseController.createCourse);
-
+router.route('/').get(courseController.getAllCourses);
+router.route('/:slug').get(courseController.getCourse);
 module.exports = router;
