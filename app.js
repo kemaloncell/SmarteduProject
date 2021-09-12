b810fe040b50ba1db5c6f5b5cc431dc9de7cf8ef
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-13 00:24:26 +0300

    Logout processes have been made and a personal page has been created.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/PageRoute');
 const courseRoute = require('./routes/CourseRoute');
 const categoryRoute = require('./routes/CategoryRoute');
@@ -25,6 +26,7 @@ app.use(
     secret: 'my_keyboard_cat',
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
   })
 );
 app.use('*', (req, res, next) => {
