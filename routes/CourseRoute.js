f1fa2e1bddf51707ce6631628063129b0eda0e18
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-13 16:51:55 +0300

    Dashborad page and user role  created

--- routes/CourseRoute.js
+++ routes/CourseRoute.js
@@ -3,7 +3,7 @@ const courseController = require('../controllers/CourseController');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const router = express.Router();
 
-router.route('/').post(roleMiddleware(['teacher,admin']), courseController.createCourse);
+router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 module.exports = router;
